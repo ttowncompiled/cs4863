52cbf48ace5a1dc19d024b717ec9fb9fc0572232
Ian Riley <ian.riley.915@gmail.com>
2016-02-07 22:07:18 -0600

    Adds pattern preprocessing.

--- zbox/zbox.go
+++ zbox/zbox.go
@@ -1,10 +1,19 @@
 package main
 
-func preprocess(pattern string) int[] {
-  
+func preprocess(pattern *string) []int {
+  zbox := make([]int, len(*pattern), len(*pattern))
+  zbox[0] = -1
+  for i := 1; i < len(*pattern); i++ {
+    zi := 0
+    for j := 0; i+j < len(*pattern) && string((*pattern)[i+j]) == string((*pattern)[j]); j++ {
+      zi++
+    }
+    zbox[i] = zi
+  }
+  return zbox
 }
 
-func match(text string, pattern string, zbox int[]) int[] {
+func match(text *string, pattern *string, zbox []int) []int {
   
 }
 
